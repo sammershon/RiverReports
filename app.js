aaa6fd4bce480998218dd545d2a1c62d36ec490f
Sam Mershon <samrmershon@gmail.com>
2016-08-23 20:29:13 -0400

    set up database connections for heroku

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 }
 else {
-  mongoose.connect('mongodb://localhost/express-movies');
+  mongoose.connect('mongodb://localhost/reports');
 }
 mongoose.connection.on('error', function(err) {
   console.error('MongoDB connection error: ' + err);
